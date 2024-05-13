b7cb7315f2ce7b0cd75e7960c703a682f278c7c7
Ivo Georgiev <ivogeorg@gmail.com>
2024-05-13 00:45:39 +0000

    initial commit

---
+++ include/velocity_library/vlib.hpp
@@ -0,0 +1,8 @@
+#ifndef VLIB_H
+#define VLIB_H
+
+#include <ros/ros.h>
+
+void display_velocity(float linear_x_vel, float angular_z_vel);
+
+#endif
---
+++ src/vlib.cpp
@@ -0,0 +1,5 @@
+#include "../include/velocity_library/vlib.hpp"
+
+void display_velocity(float linear_x_vel, float angular_z_vel) {
+  std::cout << "Linear velocity (x): " << linear_x_vel << " Angular velocity (z): " << angular_z_vel << '\n';
+}
